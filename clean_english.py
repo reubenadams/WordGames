6431883d950eee1372d4cdc7ffe49115f7419b1e
reubenadams <reubenjadams@gmail.com>
2025-01-11 18:11:42 +0000

    Implement tree node

---
+++ clean_english.py
@@ -0,0 +1,8 @@
+with open("word_lists/words_2k_most_common_contemp_fiction_wiki.txt", "rb") as f:
+    words = f.read().decode("utf-8").split("\n")
+    words = [word[3:-3] for word in words]
+    words = [word.lower() for word in words if '\'' not in word]
+
+
+with open("word_lists/words_english_2k.txt", "w") as f:
+    f.write("\n".join(words))
